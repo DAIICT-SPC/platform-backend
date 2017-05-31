0073a1e1f79da56c7b0ba6e716af115aa6782eb6
Gaurav Jiandani <jiandanigaurav@gmail.com>
2017-05-31 14:07:53 +0530

    added security medium in table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email');
+            $table->string('email')->unique();
             $table->string('password');
             $table->string('role');
             $table->string('name');
